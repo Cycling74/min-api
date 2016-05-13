d6ad717677e74fbb15e15dc73778c9f733a7bf3f
Timothy Place <tim@cycling74.com>
2016-05-13 13:16:17 -0500

    setting cmake switch for c++14

--- script/min-posttarget.cmake
+++ script/min-posttarget.cmake
@@ -2,3 +2,6 @@
 # Usage of this file and its contents is governed by the MIT License
 
 include(${C74_MAX_API_DIR}/script/max-posttarget.cmake)
+
+set_property(TARGET ${PROJECT_NAME} PROPERTY CXX_STANDARD 14)
+set_property(TARGET ${PROJECT_NAME} PROPERTY CXX_STANDARD_REQUIRED ON)
