7e6b8c4509a04c5f285e7bba5354c359ed23c182
Timothy Place <tim@cycling74.com>
2016-05-19 10:35:41 -0500

    mock: addressing security warning

--- test/mock/c74_mock_misc.h
+++ test/mock/c74_mock_misc.h
@@ -1,27 +1,26 @@
 //	Copyright 2013 - Cycling '74
 //	Timothy Place, tim@cycling74.com	
 
-#ifndef __MOCK_MISC_H__
-#define __MOCK_MISC_H__
+#pragma once
 
-
-/**	Post to the conole, mocking the cpost() function in the Max kernel.
-	This version is slightly simpler to minimize dependencies on the OS.
- */
-inline void cpost(const char *fmt, ...)
-{
-	char msg[2048+2];
-	va_list ap;
-	
-	va_start(ap, fmt);
-	vsnprintf(msg, 2048, fmt, ap);
-	va_end(ap);
-	msg[2048] = '\0';
-	printf(msg);
+namespace c74 {
+	namespace max {
+		/**	Post to the conole, mocking the cpost() function in the Max kernel.
+		 This version is slightly simpler to minimize dependencies on the OS.
+		 */
+		inline void cpost(const char *fmt, ...)
+		{
+			char msg[2048+2];
+			va_list ap;
+			
+			va_start(ap, fmt);
+			vsnprintf(msg, 2048, fmt, ap);
+			va_end(ap);
+			msg[2048] = '\0';
+			//printf(msg);
+			std::cout << msg;
+		}
+		
+	}
 }
 
-
-inline char *strncpy_zero(char *dst, const char* src, long size) { return strncpy(dst, src, size); }
-
-
-#endif // __MOCK_MISC_H__
