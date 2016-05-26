d90dedf1de79962312c8611cf95f9082e868cb74
Timothy Place <tim@cycling74.com>
2016-05-26 11:43:08 -0500

    accessories: moving random() into a `math` namespace.  adding new cosine for std::generator and mean() function as well as new filter tools for checking impulse response.

--- include/c74_min_accessories.h
+++ include/c74_min_accessories.h
@@ -9,17 +9,115 @@
 
 namespace c74 {
 namespace min {
-namespace math {
+
 	
-	/// code from http://en.cppreference.com/w/cpp/numeric/random/uniform_real_distribution
-	/// see also http://en.cppreference.com/w/cpp/numeric/random
+	namespace math {
 	
-	double random(double min, double max) {
-		std::random_device rd;
-		std::mt19937 gen(rd());
-		std::uniform_real_distribution<> dis(min, max);
-		return dis(gen);
+		/// code from http://en.cppreference.com/w/cpp/numeric/random/uniform_real_distribution
+		/// see also http://en.cppreference.com/w/cpp/numeric/random
+		
+		double random(double min, double max) {
+			std::random_device rd;
+			std::mt19937 gen(rd());
+			std::uniform_real_distribution<> dis(min, max);
+			return dis(gen);
+		}
+
+		
+		
+		
+		
+		/// Generates a cosine wave constrained between -1 to 1
+		///	@param T       render output as this datatype. algorithm was designed to assume the use of floating point.
+		///	@param size		size of the target vector
+		///	@param count	number of cycles of the wave to generate across the vector
+		
+		template <typename T>
+		class cosine {
+		public:
+			cosine (int size, double count = 1.0)
+			: mCycleSize(size)
+			, mCycleCount(count)
+			{
+				//TODO: we need way to protect against zero. static_assert did not work.
+			}
+			
+			T operator()() {
+				++mCurrent;
+				T output = std::cos((mCurrent*mCycleCount / mCycleSize) * M_PI * 2.0);
+				return output;
+			}
+			
+		private:
+			int mCurrent = -1;
+			double mCycleSize; // required by constructor
+			double mCycleCount;
+		};
+		
+		
+		template<class T>
+		auto mean(const std::vector<T>& v) {
+			double sum = std::accumulate(v.begin(), v.end(), 0.0);
+			double mean = sum / v.size();
+			
+			std::vector<double> diff(v.size());
+			std::transform(v.begin(), v.end(), diff.begin(),
+						   [mean](double x) {
+							   return x - mean;
+						   });
+			
+			double sq_sum = std::inner_product(diff.begin(), diff.end(), diff.begin(), 0.0);
+			double stdev = std::sqrt(sq_sum / v.size());
+			
+			return std::make_pair(mean, stdev);
+		}
+
+	
 	}
 
 	
-}}} // namespace c74::min::math
+	
+	namespace filters {
+	
+		/// Utility: generate an impulse response from a set of coefficients
+		/// param	a	feedforward coefficients (numerator)
+		/// param	b	feedback coefficients (denominator)
+		/// param	N	optional size of the generated response, default 64
+		auto generate_impulse_response(const samples& a, const samples& b, int N = 64) {
+			std::vector<double>	x(N);				// input -- feedforward history
+			std::vector<double>	y(N);				// output -- feedback history
+			
+			std::fill_n(x.begin(), N, 0.0);
+			std::fill_n(y.begin(), N, 0.0);
+			x[0] = 1.0;
+			
+			for (int n=0; n<N; n++) {
+				int i;
+				
+				for (i=0; i<a.size(); i++) {
+					sample	sample = x[n-i];
+					
+					if (n-i < 0 )
+						y[n] += 0.0;
+					else
+						y[n] += ( a[i] * sample );
+				}
+				
+				// ignore b[0] and assume it is normalized to 1.0
+				for (i=1; i<b.size(); i++) {
+					if (n-i < 0)
+						y[n] -= 0.0;
+					else
+						y[n] -= ( b[i] * y[n - i] );
+				}
+			}
+			return y;
+		}
+		
+	
+	
+	}
+	
+	
+
+}} // namespace c74::min
