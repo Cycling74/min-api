d625c7d1adf9dfbab27382d7a0f60956dd733785
Timothy Place <tim@cycling74.com>
2016-06-02 08:40:05 -0500

    unit test linking fixes on the Mac

--- test/min-api-unittest.cmake
+++ test/min-api-unittest.cmake
@@ -17,14 +17,16 @@ SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fprofile-arcs -ftest-cove
 
 add_executable(${PROJECT_NAME} ${PROJECT_NAME}.cpp)
 
+target_link_libraries(${PROJECT_NAME} "mock_kernel")
+
 if (APPLE)
 	target_link_libraries(${PROJECT_NAME} "-weak_framework JitterAPI")
 	set_target_properties(${PROJECT_NAME} PROPERTIES LINK_FLAGS "-Wl,-F${C74_MAX_API_DIR}/lib/mac")
 endif ()
 if (WIN32)
-	target_link_libraries(${PROJECT_NAME} ${MaxAPI_LIB})
-	target_link_libraries(${PROJECT_NAME} ${MaxAudio_LIB})
-	target_link_libraries(${PROJECT_NAME} ${Jitter_LIB})
+	#target_link_libraries(${PROJECT_NAME} ${MaxAPI_LIB})
+	#target_link_libraries(${PROJECT_NAME} ${MaxAudio_LIB})
+	#target_link_libraries(${PROJECT_NAME} ${Jitter_LIB})
 endif ()
 
 add_test(NAME ${PROJECT_NAME}
