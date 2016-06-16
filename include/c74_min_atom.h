64f884d8ba4e7a257b98bdab36384ffa504d4543
Timothy Place <tim@cycling74.com>
2016-06-15 23:30:28 -0400

    atom_reference: a new container analogous to `atoms` but for externally owned (meaning memory held elsewhere) atoms. Required to make dictionary interface work sensibly.

--- include/c74_min_atom.h
+++ include/c74_min_atom.h
@@ -8,9 +8,7 @@
 namespace c74 {
 namespace min {
 	
-	
-	class dict;
-	
+
 	class atom : public max::t_atom {
 	public:
 		
@@ -22,7 +20,11 @@ namespace min {
 		atom(const max::t_atom& init) {
 			*this = init;
 		}
-		
+
+		atom(const max::t_atom* init) {
+			*this = *init;
+		}
+
 		atom(const long value) {
 			atom_setlong(this, value);
 		}
@@ -188,37 +190,141 @@ namespace min {
 		s = value;
 		return *this;
 	}
-
 	
+
+	/// The #atoms container is the standard means by which zero or more values are passed.
+	/// It is implemented as a std::vector of the #atom type, and thus atoms contained in an
+	/// #atoms container are 'owned' copies... not simply a reference to some externally owned atoms.
+
+	// TODO: how to document inherited interface, e.g. size(), begin(), etc. ?
 	
 	using atoms = std::vector<atom>;
+
+
+#ifdef __APPLE__
+#pragma mark -
+#pragma mark AtomRef
+#endif
 	
-	std::string atoms_to_string(const atoms& as) {
-		long		textsize = 0;
-		char*		text = nullptr;
-		std::string	str;
+	
+	/// The #atom_reference type defines a container for atoms by reference, providing an interface
+	/// that is interoperable with any of the classic standard library containers.
+	///
+	/// Typically you *do not use* the #atom_reference type explicitly.
+	/// It is rather intended as an intermediary between the #atoms container type and
+	/// old C-style functions in the Max API.
+	/// As such it resembles some of the aims of the gsl::span type but serving a much more specialized purpose.
+	///
+	/// IMPORTANT: The size and order of members in this class are designed to make it a drop-in replace for
+	/// the old C-style argc/argv pairs!  As such, no changes or additions should be made with regards to
+	/// members, virtual methods, etc.
+	
+	class atom_reference {
+	public:
 		
-		auto err = max::atom_gettext(as.size(), &as[0], &textsize, &text, max::OBEX_UTIL_ATOM_GETTEXT_SYM_NO_QUOTE);
+		using size_type			= long;
+		using value_type		= max::t_atom*;
+		using iterator			= max::t_atom*;
+		using const_iterator	= const max::t_atom*;
+		
+		iterator begin()				{ return av; }
+		const_iterator begin() const	{ return av; }
+		iterator end()					{ return av+ac; }
+		const_iterator end() const		{ return av+ac; }
+		
+		size_type size() const			{ return ac; }
+		bool empty() const				{ return size()==0; }
+		
+		// We don't own the array of atoms, so we cannot do these operations:
+		// insert
+		// erase
+		// push_back
+		// push_front
+		// pop_front
+		// pop_back
+		
+		// TODO: we could consider implementing the following,
+		// but it is not clear we need them due to the limited roll of this type:
+		// front()
+		// back()
+		// operator []
+		// at()
+		
+		
+		atom_reference(long argc, max::t_atom* argv)
+		: ac(argc)
+		, av(argv)
+		{}
+		
+		atom_reference& operator = (const symbol& value) {
+			ac = 1;
+			atom_setsym(av, value);
+			return *this;
+		}
+		
+		operator atom() const {
+			if (empty())
+				throw std::out_of_range("atomref is empty");
+			return atom(av);
+		}
+		
+		operator atoms() const {
+			c74::min::atoms as;
+			
+			for (auto i=0; i<ac; ++i)
+				as.push_back(av+i);
+			
+			return as;
+		}
+		
+		
+	private:
+		long			ac;
+		max::t_atom*	av;
+	};
+	
+}} // namespace c74::min
+
+
+
+#ifdef __APPLE__
+#pragma mark -
+#pragma mark Utilities
+#endif
+
+
+namespace std {
+	
+	// overloads of the std::to_string() function for our min::atoms type
+	// it is perfectly legal to make this overload in the std namespace because it is overloaded on our user-defined type
+	// as stated in section 17.6.4.2.1 of working draft version N4296 of the C++ Standard at
+	// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2014/n4296.pdf
+	
+	
+	string to_string(const c74::min::atoms& as) {
+		long	textsize = 0;
+		char*	text = nullptr;
+		string	str;
+		
+		auto err = c74::max::atom_gettext(as.size(), &as[0], &textsize, &text, c74::max::OBEX_UTIL_ATOM_GETTEXT_SYM_NO_QUOTE);
 		if (!err)
 			str = text;
 		else
-			max::object_error(nullptr, "problem geting text from atoms");
+			c74::max::object_error(nullptr, "problem geting text from atoms");
 		
 		if (text)
-			max::sysmem_freeptr(text);
+			c74::max::sysmem_freeptr(text);
 		
 		return str;
 	}
 	
 	
-	
-	// TODO: move the atoms on return instead of copy
-	atoms atoms_from_acav(long ac, max::t_atom* av) {
-		atoms as;
+	string to_string(const c74::min::atom_reference& ar) {
+		c74::min::atoms as;
+		for (const auto& ref: ar)
+			as.push_back(ref);
 		
-		for (int i=0; i<ac; ++i)
-			as.push_back(av[i]);
-		return as;
-	}	
+		return to_string(as);
+	}
 	
-}} // namespace c74::min
+}
