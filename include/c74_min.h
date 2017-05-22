ec2bcdb27ce999b82a5dcfeba56d4581fdedbeb5
Timothy Place <tim@cycling74.com>
2017-05-22 08:27:11 -0600

    windows build fixes for packages requiring the new Min Lib

--- include/c74_min.h
+++ include/c74_min.h
@@ -12,8 +12,12 @@
 #include "c74_min_attribute_impl.h"
 #include "c74_min_buffer_impl.h"
 
-#if __has_include("../../min-lib/include/c74_lib.h")
-#include "../../min-lib/include/c74_lib.h"
-#endif
+
+// TODO: The use of __has_include on Windows requires VS version 15.3 which is not yet available
+//#if __has_include("../../min-lib/include/c74_lib.h")
+	#include "../../min-lib/include/c74_lib.h"
+//#endif
+
 
 #undef C74_MIN_WITH_IMPLEMENTATION
+
