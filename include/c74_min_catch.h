7937d90ec268a890eadd5ca30f558daae32cf4c8
Timothy Place <tim@cycling74.com>
2017-07-20 13:30:26 -0600

    c74_min_catch.h: documentation

--- include/c74_min_catch.h
+++ include/c74_min_catch.h
@@ -5,9 +5,17 @@
 
 #pragma once
 
-#include "catch.hpp"
-#include "c74_min.h"
+#include "catch.hpp"    // The Catch header must come first -- otherwise some C++ includes will tangle with it and cause problems.
+#include "c74_min.h"    // The standard Min header
 
+
+/// Compare a container (e.g. a vector) of floats in a Catch unit test.
+/// If there is a failure, not only will the return value be false, but a Catch REQUIRE will fail.
+///
+/// @tparam T           The type for comparisons. Should be an STL container of floats or doubles.
+/// @param  source      The values to compare
+/// @param  reference   The reference values against which to compare.
+/// @return             True if they are (approximately) the same. Otherwise false.        
 
 template<typename T>
 bool require_vector_approx(T source, T reference) {
@@ -23,5 +31,13 @@ bool require_vector_approx(T source, T reference) {
 	return true;
 }
 
+
+/// Compare a container (e.g. a vector) of floats in a Catch unit test.
+/// If there is a failure, not only will the return value be false, but a Catch REQUIRE will fail.
+///
+/// @tparam T           The type for comparisons. Should be an STL container of floats or doubles.
+/// @param  source      The values to compare
+/// @param  reference   The reference values against which to compare.
+/// @return             True if they are (approximately) the same. Otherwise false.        
 
 #define REQUIRE_VECTOR_APPROX( source, reference ) require_vector_approx(source, reference)
