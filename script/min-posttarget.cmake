e181e6a80d79a3fc418194bd727a550099fb8b1d
Timothy Place <tim@cycling74.com>
2017-08-10 10:11:59 -0500

    cmake: stricter warnings on the mac

--- script/min-posttarget.cmake
+++ script/min-posttarget.cmake
@@ -5,3 +5,9 @@ include(${C74_MAX_API_DIR}/script/max-posttarget.cmake)
 
 set_property(TARGET ${PROJECT_NAME} PROPERTY CXX_STANDARD 14)
 set_property(TARGET ${PROJECT_NAME} PROPERTY CXX_STANDARD_REQUIRED ON)
+
+if (APPLE)
+    # enforce a strict warning policy
+    set_target_properties(${PROJECT_NAME} PROPERTIES XCODE_ATTRIBUTE_WARNING_CFLAGS "-Wall -Werror -Wmissing-field-initializers")    
+    # -Wmost -Wno-four-char-constants -Wno-unknown-pragmas $(inherited)
+endif ()
