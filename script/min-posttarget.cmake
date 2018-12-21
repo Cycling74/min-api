276abbf960108257ef2d580c6ea12711003f24eb
Timothy Place <tim@cycling74.com>
2018-12-21 11:52:14 -0600

    cmake: -Werror flag is only enabled as a default for objects in the Min-DevKit as it makes for a brutal dev experience outside of that context. see Cycling74/min-devkit#95.

--- script/min-posttarget.cmake
+++ script/min-posttarget.cmake
@@ -7,7 +7,13 @@ set_property(TARGET ${PROJECT_NAME} PROPERTY CXX_STANDARD 14)
 set_property(TARGET ${PROJECT_NAME} PROPERTY CXX_STANDARD_REQUIRED ON)
 
 if (APPLE)
+    if ("${PROJECT_NAME}" MATCHES "min.")
+        set(MAKE_ALL_WARNINGS_ERRORS "-Werror")
+    else ()
+        set(MAKE_ALL_WARNINGS_ERRORS "")
+    endif ()
+
     # enforce a strict warning policy
-    set_target_properties(${PROJECT_NAME} PROPERTIES XCODE_ATTRIBUTE_WARNING_CFLAGS "-Wall -Werror -Wmissing-field-initializers -Wno-unused-lambda-capture -Wno-unknown-warning-option")    
+    set_target_properties(${PROJECT_NAME} PROPERTIES XCODE_ATTRIBUTE_WARNING_CFLAGS "-Wall ${MAKE_ALL_WARNINGS_ERRORS} -Wmissing-field-initializers -Wno-unused-lambda-capture -Wno-unknown-warning-option")    
     # -Wmost -Wno-four-char-constants -Wno-unknown-pragmas $(inherited)
 endif ()
