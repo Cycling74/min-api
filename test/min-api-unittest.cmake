9772dd7d3fe9cb3ee09c51b259ab7f37da4badba
Alex Norman <xnor@cycling74.com>
2020-10-02 14:40:37 -0700

    update the location of the tests to be in the build dir

--- test/min-api-unittest.cmake
+++ test/min-api-unittest.cmake
@@ -46,7 +46,7 @@ if (WIN32)
 	add_custom_command(	TARGET ${PROJECT_NAME} 
 						POST_BUILD	# Adds a post-build event to MyTest
     					COMMAND ${CMAKE_COMMAND} -E copy_if_different		# which executes "cmake - E copy_if_different..."
-        				"${CMAKE_CURRENT_SOURCE_DIR}/../../../../tests/mock_kernel.dll"	# <--this is in-file
+					"${CMAKE_BINARY_DIR}/tests/mock_kernel.dll"	# <--this is in-file
        				 	${CMAKE_CURRENT_BINARY_DIR}                 					# <--this is out-file path
 )
 endif ()
