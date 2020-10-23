caded99d81f81b80f6630e863e7f84d1655c15a9
Hidde de Jong <hidde@cycling74.com>
2020-10-23 22:10:46 +0200

    Cleanup: spacing

--- test/min-object-unittest.cmake
+++ test/min-object-unittest.cmake
@@ -6,15 +6,15 @@ cmake_minimum_required(VERSION 3.10)
 set(ORIGINAL_NAME "${PROJECT_NAME}")
 set(TEST_NAME "${PROJECT_NAME}_test")
 
-if (EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${TEST_NAME}.cpp")
+if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${TEST_NAME}.cpp")
 
 	enable_testing()
 
-	include_directories( 
+	include_directories(
 		"${C74_INCLUDES}"
 		"${C74_MIN_API_DIR}/test"
 	)
-    
+
 	add_definitions(
 		-DMIN_TEST
 	)
@@ -22,20 +22,20 @@ if (EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${TEST_NAME}.cpp")
 	set(TEST_SOURCE_FILES "")
 	foreach(SOURCE_FILE ${SOURCE_FILES})
 		set(ORIGINAL_WITH_EXT "${ORIGINAL_NAME}.cpp")
-		if (SOURCE_FILE STREQUAL ORIGINAL_WITH_EXT)
+		if(SOURCE_FILE STREQUAL ORIGINAL_WITH_EXT)
 			set(TEST_SOURCE_FILES ${TEST_SOURCE_FILES} ${TEST_NAME}.cpp)
 		else()
 			set(TEST_SOURCE_FILES "${TEST_SOURCE_FILES}" ${SOURCE_FILE})
 		endif()
 	endforeach()
-	
+
 	set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/../../../tests")
 	set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}")
 	set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}")
 
-	if (APPLE)
+	if(APPLE)
 		set(CMAKE_OSX_ARCHITECTURES x86_64)
-	endif ()
+	endif()
 
 	add_executable(${TEST_NAME} ${TEST_NAME}.cpp ${TEST_SOURCE_FILES})
 
@@ -44,16 +44,14 @@ if (EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${TEST_NAME}.cpp")
 
     target_link_libraries(${TEST_NAME} PUBLIC "mock_kernel")
 
-	if (APPLE)
+	if(APPLE)
         set_target_properties(${TEST_NAME} PROPERTIES LINK_FLAGS "-Wl,-F'${C74_MAX_API_DIR}/lib/mac', -weak_framework JitterAPI")
 		target_compile_options(${TEST_NAME} PRIVATE -DCATCH_CONFIG_NO_CPP17_UNCAUGHT_EXCEPTIONS)
-	endif ()
-	if (WIN32)
+	elseif(WIN32)
         set_target_properties(${TEST_NAME} PROPERTIES COMPILE_PDB_NAME ${TEST_NAME})
-	endif ()
+	endif()
 
 	add_test(NAME ${TEST_NAME}
 	         COMMAND ${TEST_NAME})
-	 
-endif ()
-	 
+
+endif()
