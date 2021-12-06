016d0d9446d630f4d3d69844912ed6c61164900b
Isabel Kaspriskie <isabel@cycling74.com>
2021-12-06 15:29:41 -0500

    Add method to register an existing dictionary

--- include/c74_min_dictionary.h
+++ include/c74_min_dictionary.h
@@ -136,6 +136,17 @@ namespace c74::min {
         }
 
 
+		/// Register an existing dictionary
+		/// @param name   name to register the dictionary under
+		void register_as(const symbol name) {
+			auto d_name = max::dictobj_namefromptr(m_instance);
+			if (!d_name && m_instance != nullptr) {
+				max::t_symbol* s = name;
+				max::dictobj_register(m_instance, &s);
+			}
+		}
+
+
     private:
         max::t_dictionary* m_instance       { nullptr };
         bool               m_has_ownership  { true };
