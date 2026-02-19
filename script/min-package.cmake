fd99e1dc3fbc2becdc2e88748af3a5701fb695b1
Isabel Kaspriskie <mail@isabelgk.com>
2026-02-19 11:38:46 -0500

    Use FetchContent for max-sdk-base

--- script/min-package.cmake
+++ script/min-package.cmake
@@ -1,4 +1,12 @@
 # Copyright 2018 The Min-API Authors. All rights reserved.
 # Use of this source code is governed by the MIT License found in the License.md file.
 
-include(${CMAKE_CURRENT_LIST_DIR}/../max-sdk-base/script/max-package.cmake)
+include(FetchContent)
+FetchContent_Declare(
+    max-sdk-base
+    GIT_REPOSITORY https://github.com/Cycling74/max-sdk-base.git
+    GIT_TAG        f3e555a5069c0efecc53135da30994ccbb869f90
+)
+FetchContent_MakeAvailable(max-sdk-base)
+
+include(${MAXSDK_BASE_DIR}/script/max-package.cmake)
